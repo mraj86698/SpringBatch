cf953de1d198c9819099f79b55d06051c4ac0668
MohanRaj <mraj86698@gmail.com>
2022-11-17 08:46:24 +0530

    Spring Batch  using to read a CSV File And Write that csv File back to the database

---
+++ src/main/java/com/example/demo/Model/User.java
@@ -0,0 +1,39 @@
+package com.example.demo.Model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+	@Id
+	
+	private Integer id;
+	private String name;
+	private String dept;
+	private Integer salary;
+	private Date time;
+
+	@Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("User{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", dept='").append(dept).append('\'');
+        sb.append(", salary=").append(salary);
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
